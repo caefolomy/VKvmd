3b6175381de2a8436acbf95c8255261ff69e32a3
caefolomy <107803034+caefolomy@users.noreply.github.com>
2026-04-24 01:50:13 +0300

    personal.ps1

--- personal.ps1
+++ personal.ps1
@@ -1,13 +1,14 @@
+# Не забудьте в первом шаге заменить заглушку `"doc$id_собеседника"`, например `"doc$1"`.
 # 1. Находение и оставление в документе строки содержащей только документы от собеседника: 
 Get-Content messages.txt | Where-Object { $_ -match "doc$id_собеседника" } | Set-Content doc_messages.txt
 
-# 2. Находение и оставление в документе только URL-ссылки голосовых сообщений от собеседника:
+# 2. Находение и оставление в документе строки содержащей только документы от собеседника:
 Get-Content doc_messages.txt |
     Where-Object { $_ -match 'https:.*\.ogg' } |
     ForEach-Object { $_ -replace '.*?(https:.*?\.ogg).*', '$1' } |
     Set-Content voice_messages.txt
 
-# 3. Загрузка .ogg файлов:
+# 3. Находение и оставление в документе только URL-ссылки голосовых сообщений от собеседника:
 Get-Content voice_messages.txt | ForEach-Object {
     $filename = [System.IO.Path]::GetFileName($_)
     Invoke-WebRequest -Uri $_ -OutFile $filename
